e5abf54aaa4c27097e0473a3e57e9ba39d162f69
Alicegif <alicegif@gmail.com>
2024-11-22 20:38:16 -0600

    Add files via upload
    fixing the build problem

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='pytorch_ehr',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Copied from the Zhi group',
+    author='Alice Gifford branched on github',
+    license='MIT',
+)
